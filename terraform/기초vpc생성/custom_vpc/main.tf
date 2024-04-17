79ad1771f3e7435fc0a6754f5a5820f531c7bf13
xoodb <xodbs316@google.com>
2024-04-17 23:15:15 +0900

    add rt

--- terraform/기초vpc생성/custom_vpc/main.tf
+++ terraform/기초vpc생성/custom_vpc/main.tf
@@ -27,12 +27,59 @@ resource "aws_subnet" "private_subnet_1" {
   }
 }
 
-resource "aws_nat_gateway" "private_nat" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.private_subnet_1.id
-}
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.default_vpc.id
+
+  tags = {
+    Name = "xoodb_default_vpc_IGW"
+  }
 }
 
+resource "aws_eip" "nat_ip" {
+  domain   = "vpc"
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "private_nat" {
+  allocation_id     = aws_eip.nat_ip.id
+  subnet_id         = aws_subnet.private_subnet_1.id
+  tags = {
+    Name = "xoodb_NAT"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.default_vpc.id
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route" "public_worldwide" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_rt_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id   = aws_vpc.default_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.private_nat.id
+  }
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
